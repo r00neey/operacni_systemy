43184ca209836a28b4d705035e590953eda39275
r00neey <98259422+r00neey@users.noreply.github.com>
2024-04-27 20:13:31 +0200

    Update main.c

--- OS1.8/OS1.8/main.c
+++ OS1.8/OS1.8/main.c
@@ -6,15 +6,15 @@ int _tmain(int argc, TCHAR* argv[]) {
 	//vytvoreni handlu, ktery vytvori nebo otevre soubor
 	HANDLE hFileZapis = CreateFile( 
 		_T("soubor.txt"),						//cesta k souboru
-		GENERIC_READ | GENERIC_WRITE,			//rezim prace se souborem
+		GENERIC_READ | GENERIC_WRITE,					//rezim prace se souborem
 		FILE_SHARE_READ,						//rezim sdileneho pristupu k soubor (jen pro cteni)
-		NULL,									//bezpecnostni atributy
+		NULL,								//bezpecnostni atributy
 		OPEN_ALWAYS,							//jak nalozit s (ne)existujicimi soubory
-		0,										//priznaky souboru
-		NULL);									//odkaz na sablonu
+		0,								//priznaky souboru
+		NULL);								//odkaz na sablonu
 
-	if (hFileZapis == INVALID_HANDLE_VALUE) {	//protoze handle nevraci v pripade chyby null
-		printf("error opening file\n");			//ale INVALID_HANDLE_VALUE	
+	if (hFileZapis == INVALID_HANDLE_VALUE) {				//protoze handle nevraci v pripade chyby null
+		printf("error opening file\n");					//ale INVALID_HANDLE_VALUE	
 	}
 
 	_TCHAR* str = _T("cau svete!");
@@ -32,7 +32,7 @@ int _tmain(int argc, TCHAR* argv[]) {
 		_T("soubor.txt"),						//cesta k souboru		
 		GENERIC_READ,							//access mode
 		FILE_SHARE_READ,						//rezim sdileneho pristupu k souboru
-		NULL,									//bezpecnostni atributy
+		NULL,								//bezpecnostni atributy
 		OPEN_EXISTING,							//otevreni, pokud soubor existuje
 		0,										
 		NULL);
@@ -47,16 +47,16 @@ int _tmain(int argc, TCHAR* argv[]) {
 	DWORD dwBytesRead = 0;
 
 	BOOL precteniFile = ReadFile(
-		hFileCteni,								//file handle
-		buffer,									//buffer s prectenym textem
+		hFileCteni,							//file handle
+		buffer,								//buffer s prectenym textem
 		dwBytesToRead,							//buffer size-1
 		&dwBytesRead,							//pocet prectenych bajtu
 		NULL);
 
 	if (dwBytesRead > 0) {
-		printf("%s", buffer);					//vypis bufferu
+		printf("%s", buffer);						//vypis bufferu
 	}
 
-	CloseHandle(hFileCteni);					//zavreni handlu
+	CloseHandle(hFileCteni);						//zavreni handlu
 	return 0;
 }
